cc36cad0bba478aa87465e3da2ed3a5154de3e9b
bruno <bmassarelli@gmail.com>
2026-04-14 16:23:16 -0300

    feat: add Navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-primary">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">Subscription Manager</span>
+      </div>
+    </nav>
+  );
+}
